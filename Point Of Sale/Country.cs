4fa1ddcebca5849452a3c41f6fc55fdc8f2fe0d5
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-02-22 23:33:01 +0600

    Update City.cs, Country.cs, EmployeeAdmin.cs

--- Point Of Sale/Country.cs
+++ Point Of Sale/Country.cs
@@ -11,7 +11,8 @@ namespace Point_Of_Sale
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Country
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,10 @@ namespace Point_Of_Sale
         }
     
         public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(15,MinimumLength = 2)]
         public string CountryName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
